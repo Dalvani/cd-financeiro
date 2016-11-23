9e366b3eae20273dfe5999dc69f12362f9646081
vagrant <vagrant@homestead>
2016-11-23 16:00:30 +0000

    Primeiras Autenticações

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\CodeFin\User::class,1)
+            ->states('admin')
+            ->create([
+                'email' => 'admin@user.com',
+                'name' => 'Adminstrador Teste'
+        ]);
+
+        factory(\CodeFin\User::class,1)
+            ->create([
+                'email' => 'client@user.com',
+                'name' => 'Cliente Teste'
+        ]);
+    }
+}
